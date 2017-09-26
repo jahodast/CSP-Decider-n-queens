0c0393f380d31ed3e72ba3d249ed0b018329a3c6
stanislavj <s.jahoda@weekamp-doors.cz>
2017-09-26 13:10:30 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace n_queens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
